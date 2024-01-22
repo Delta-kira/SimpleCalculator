2234d6a4fc94be1bf2416851ec61c8878bcdcd7e
DELTAKIRA <mrtigeryt2009@gmail.com>
2024-01-22 15:40:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/calculator/CalculatorOperation.kt
@@ -0,0 +1,8 @@
+package com.example.calculator
+
+sealed class CalculatorOperation(val symbol : String){
+    object Add : CalculatorOperation("+")
+    object Subtract : CalculatorOperation("-")
+    object Multiply : CalculatorOperation("X")
+    object Divide : CalculatorOperation("/")
+}
